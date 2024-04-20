1c47e401912094d91bbd57b10a425b6ae2475c50
Negar <negarsaljooghi@gmail.com>
2024-04-20 09:40:47 +0330

    mouse move event

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,11 @@ const copy = document.querySelector('.copy-me');
 copy.addEventListener('copy', () => {
     console.log('OI! my content is copyright');
 });
+
+// mouse move event
+const box = document.querySelector('.box');
+
+box.addEventListener('mousemove', e => {
+    //console.log(e.offsetX, e.offsetY);
+    box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`;
+});
